7a493ca5f20f78234c7142ce3d00c4165a0d6cb1
JDZ.Chao <jdzchao@gmail.com>
2017-08-01 10:34:16 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="app">
+    <three></three>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+  import Three from './components/Three.vue'
+
   export default {
-    name: 'app'
+    name: 'app',
+    components: {Three}
   }
 </script>
 
 <style>
-
+  html body {
+    margin: 0;
+    padding: 0;
+    font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  }
 </style>
---
+++ src/components/Three.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="Three">ddd</div>
+</template>
+
+<script>
+  import Three from 'three';
+
+  export default {
+    name: 'Three',
+    data() {
+      return {}
+    }
+  }
+</script>
+
+
+<style scoped>
+
+</style>
