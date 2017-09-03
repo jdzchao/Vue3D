49080af649764f9336f87f433230fcb3e6168727
JDZ.Chao <jdzchao@gmail.com>
2017-09-03 11:15:35 +0800

    架构完成，准备进行组件开发！

---
+++ src/components/XObjLoader.vue
@@ -0,0 +1,19 @@
+<template>
+</template>
+<script>
+  const THREE = require('three');
+  THREE.ObjLoader = require('imports-loader?THREE=three!exports-loader?THREE.OrbitControls!../../node_modules/three/examples/js/loaders/OBJLoader');
+  import _XCom from './_XCom.vue'
+
+  export default {
+    name: 'XObjLoader',
+    mixins: [_XCom],
+    props: {
+      path: {type: String},
+      name: {type: String, default: 'main'},
+    },
+    data() {
+      return {}
+    }
+  }
+</script>
