25732c20f16ed3c99fa688181c9dc72716d25d61
Li Chao <jdzchao@gmail.com>
2017-12-23 09:42:48 +0800

    Revert "add panorama scene"
    This reverts commit fc70b4a8dc8c9e73da5b1e4e518afeca87ab9519.

--- Vue3D/index.js
+++ Vue3D/index.js
@@ -4,7 +4,6 @@ import XBoxGeometry from './packages/XBoxGeometry'
 import XCamera from './packages/XCamera'
 import XLight from './packages/XLight'
 import XObjLoader from './packages/XObjLoader'
-import VPanoramaScene from './packages/VPanoramaScene'
 
 import Materials from './packages/Materials'
 import Utils from './packages/Utils'
@@ -16,7 +15,6 @@ export {
   XCamera,
   XLight,
   XObjLoader,
-  VPanoramaScene,
   Utils,
   Materials
 };
@@ -29,7 +27,6 @@ const Vue3D = {
   install: function (Vue, options) {
     Vue.prototype.$vue3d = $vue3d;
     Vue.component('v-scene', VScene);
-    Vue.component('v-panorama-scene', VPanoramaScene);
     Vue.component('w-orbit-controls', WOrbitControls);
     Vue.component('x-box-geometry', XBoxGeometry);
     Vue.component('x-camera', XCamera);
