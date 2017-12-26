713020d022abd4f43e2cb677033bd05f8d1b803b
JdzChao <jdzchao@qq.com>
2017-12-26 17:07:39 +0800

    检测射线事件，拖动时不触发射线

--- Vue3D/packages/WRaycast/WRaycast.vue
+++ Vue3D/packages/WRaycast/WRaycast.vue
@@ -13,19 +13,30 @@
       return {
         raycaster: new THREE.Raycaster(),
         mouse: new THREE.Vector2(),
-        target: []
+        target: [],
+        isClick: false,
       }
     },
     created() {
-      this.root.dom.addEventListener('click', this.caster, false);
+      this.root.dom.addEventListener('mousedown', this.mouseLock, false);
+      this.root.dom.addEventListener('mousemove', this.dragLock, false);
+      this.root.dom.addEventListener('mouseup', this.caster, false);
     },
     methods: {
       caster(event) {
+        if (!this.isClick) return;
         this.mouse.x = (event.clientX / this.root.dom.clientWidth) * 2 - 1;
         this.mouse.y = -(event.clientY / this.root.dom.clientHeight) * 2 + 1;
         this.raycaster.setFromCamera(this.mouse, this.root.camera);
         this.target = this.raycaster.intersectObjects(this.root.scene.children, true);
         this.$emit('cast', this.target);
+        this.isClick = true;
+      },
+      mouseLock() {
+        this.isClick = true;
+      },
+      dragLock() {
+        this.isClick = false;
       }
     }
   }
