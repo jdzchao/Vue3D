87be6185d38c2beed3dff5a08aac50b723a13b3d
JDZChao <jdzchao@gmail.com>
2019-04-27 14:24:32 +0800

    add Demo Models

--- src/components/demo/DemoDev.vue
+++ src/components/demo/DemoDev.vue
@@ -5,20 +5,18 @@
                 <v3d-light-rect-area :width="100" :height="100" :intensity="1"
                                      :target="{x:5,y:0,z:0}" :position="{x:0,y:0,z:10}"></v3d-light-rect-area>
                 <!--                <v3d-geom-cylinder :material="Materials.standard()" :radialSegments="50"></v3d-geom-cylinder>-->
-                <v3d-loader-obj path="/objects/WaltHead.obj" :scale="{x:0.1,y:0.1,z:0.1}"
-                                :material="Materials.standard()" :position="{x:0,y:0,z:0}"></v3d-loader-obj>
+                <v3d-loader-obj path="/models/obj/Cerberus.obj"
+                                :material="Materials.standard()"></v3d-loader-obj>
             </v3d-scene>
         </vue3d>
     </div>
 </template>
 
 <script>
-    import {Bus, Materials} from '@v3d'
-    import V3dLoaderObj from "@v3d/packages/V3dLoaderObj/V3dLoaderObj";
+    import {Materials} from '@v3d'
 
     export default {
         name: "DemoDev",
-        components: {V3dLoaderObj},
         data() {
             return {
                 width: 100,
