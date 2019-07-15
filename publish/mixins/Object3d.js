21885b3a3e4c52a01e50ec2fa476c34053e0bd6b
JDZChao <jdzchao@gmail.com>
2019-07-15 10:19:10 +0800

    0.8.0 修正组件数据同步问题

--- publish/mixins/Object3d.js
+++ publish/mixins/Object3d.js
@@ -1,44 +1,42 @@
 /**
  * 所有mixins需要在mounted之前创建Object3d对象
  */
+import * as THREE from 'three'
+
 export default {
     name: "Object3d",
     props: {
         inTree: {type: Boolean, default: true}, // 是否挂载在树节点上。当设置为false时直接挂载在scene根节点上
+        name: {type: String, default: ''},
         position: {
-            type: Object,
-            default: () => {
-                return {x: 0, y: 0, z: 0}
+            type: Object, default() {
+                return new THREE.Vector3()
             }
         },
         rotation: {
-            type: Object,
-            default: () => {
-                return {x: 0, y: 0, z: 0}
+            type: Object, default() {
+                return new THREE.Euler()
             }
         },
         scale: {
-            type: Object,
-            default: () => {
-                return {x: 1, y: 1, z: 1}
+            type: Object, default() {
+                return new THREE.Vector3(1, 1, 1)
             }
         },
         target: {
             type: Object,
-            default: () => {
+            default() {
                 return {x: 0, y: 0, z: 0}
             }
         },
-        name: {
-            type: String,
-        }
     },
     data() {
         return {
             vue3d: null, // Vue3D component
             scene: null, // mounted scene
-            object3d: null, // 当前三维对象
             parent: null, // 父级三维对象
+            object3d: null, // 当前三维对象
+
             slot: false, // 是否挂载子节点
             visible: true, // 是否可见
         }
@@ -61,21 +59,23 @@ export default {
         this.render();
     },
     beforeDestroy() {
-        this.onDestroy();
-        this.slotOut();
-        this._removeObject3d(this.object3d);
+        this.$emit('remove', this.object3d);
+        this.slot = false;
+        this.removeObject3d(this.object3d);
     },
     methods: {
         init() {
             this.object3d.vComponent = this;
             this.object3d.name = this.name || this.$options.name;
-            this.setPosition();
-            this.setRotation();
-            this.setScale();
+
+            this.addObject3d(this.object3d);
+            this.setPosition(this.position);
+            this.setRotation(this.rotation);
+            this.setScale(this.scale);
             this.setTarget();
-            this._addObject3d(this.object3d);
-            this.slotIn();
-            this.onReady();
+
+            this.slot = true;
+            this.$emit('ready', this.object3d);
             this.render();
         },
         // 根据vue组件递归查询scene节点
@@ -96,122 +96,83 @@ export default {
                 this._recursion(parent.$parent);
             }
         },
-        // 插槽中添加三维对象
-        _addObject3d(object3d) {
-            if (this.inTree) {
-                this.addObject3d(object3d, true);
-            } else {
-                this.addObject3d(object3d, false);
-            }
-        },
-        // 插槽中移除三位对象
-        _removeObject3d(object3d) {
-            this.removeObject3d(object3d, true);
-        },
-        setPosition() {
-            this.object3d.position.set(this.position.x, this.position.y, this.position.z);
-            this.render();
-        },
-        setRotation() {
-            this.object3d.rotation.set(this.rotation.x, this.rotation.y, this.rotation.z);
-            this.render();
-        },
-        setScale() {
-            this.object3d.scale.set(this.scale.x, this.scale.y, this.scale.z);
-            this.render();
-        },
-        setTarget() {
-            this.object3d.lookAt(this.target.x, this.target.y, this.target.z);
-        },
-        addObject3d(object3d, inNode) {
-            if (inNode && this.parent.hasOwnProperty('object3d')) {
+        addObject3d(object3d) {
+            if (this.inTree && this.parent.hasOwnProperty('object3d')) {
                 this.parent.object3d.add(object3d);
             } else {
                 this.scene.add(object3d);
             }
         },
-        removeObject3d(object3d, inNode) {
-            if (inNode && this.parent.hasOwnProperty('object3d')) {
+        removeObject3d(object3d) {
+            if (this.inTree && this.parent.hasOwnProperty('object3d')) {
                 this.parent.object3d.remove(object3d);
             } else {
-                this.removeObject3d(object3d);
+                this.scene.remove(object3d);
             }
             this.vue3d.off("update", this.onRender);
         },
-        // 激活插槽
-        slotIn() {
-            this.slot = true;
+        setPosition(val) {
+            if (val.isVector3) {
+                this.object3d.position = val;
+            } else if (val && val.hasOwnProperty('x') && val.hasOwnProperty('y') && val.hasOwnProperty('z')) {
+                let position = new THREE.Vector3(val.x, val.y, val.z);
+                this.object3d.position = position;
+                this.$emit("update:position", position);
+            }
+            this.render();
         },
-        // 屏蔽插槽
-        slotOut() {
-            this.slot = false;
+        setRotation(val) {
+            if (val.isEuler) {
+                this.object3d.rotation = val;
+            } else if (val && val.hasOwnProperty('x') && val.hasOwnProperty('y') && val.hasOwnProperty('z')) {
+                let rotation = new THREE.Euler(val.x, val.y, val.z);
+                this.object3d.rotation = rotation;
+                this.$emit("update:rotation", rotation);
+            }
+            this.render();
+        },
+        setScale(val) {
+            if (val.isVector3) {
+                this.object3d.scale = val;
+            } else if (val && val.hasOwnProperty('x') && val.hasOwnProperty('y') && val.hasOwnProperty('z')) {
+                let scale = new THREE.Vector3(val.x, val.y, val.z);
+                this.object3d.scale = scale;
+                this.$emit("update:scale", scale);
+            }
+            this.render();
+        },
+        setTarget() {
+            this.object3d.lookAt(this.target.x, this.target.y, this.target.z);
+            this.render();
         },
         // 渲染
         render() {
             this.vue3d.render();
         },
-        onReady() {
-            this.$emit('ready', this.object3d);
-        },
         onRender() {
             this.$emit('update', this.object3d);
         },
-        onDestroy() {
-            this.$emit('remove', this.object3d);
-        }
     },
     watch: {
         name(val, oldVal) {
             if (val === oldVal) return;
             this.object3d.name = val;
         },
-        "position.x"(val, oldVal) {
-            if (val === oldVal) return;
-            this.setPosition();
-        },
-        "position.y"(val, oldVal) {
-            if (val === oldVal) return;
-            this.setPosition();
-        },
-        "position.z"(val, oldVal) {
-            if (val === oldVal) return;
-            this.setPosition();
-        },
-        "rotation.x"(val, oldVal) {
-            if (val === oldVal) return;
-            this.setRotation();
-        },
-        "rotation.y"(val, oldVal) {
-            if (val === oldVal) return;
-            this.setRotation();
-        },
-        "rotation.z"(val, oldVal) {
-            if (val === oldVal) return;
-            this.setRotation();
-        },
-        "scale.x"(val, oldVal) {
-            if (val === oldVal) return;
-            this.setScale();
-        },
-        "scale.y"(val, oldVal) {
-            if (val === oldVal) return;
-            this.setScale();
-        },
-        "scale.z"(val, oldVal) {
-            if (val === oldVal) return;
-            this.setScale();
-        },
-        "target.x"(val, oldVal) {
+        target(val, oldVal) {
             if (val === oldVal) return;
             this.setTarget();
         },
-        "target.y"(val, oldVal) {
-            if (val === oldVal) return;
-            this.setTarget();
+        position(val) {
+            if (Object.is(this.object3d.position, val)) return;
+            this.setPosition(val);
         },
-        "target.z"(val, oldVal) {
-            if (val === oldVal) return;
-            this.setTarget();
+        rotation(val) {
+            if (Object.is(this.object3d.rotation, val)) return;
+            this.setRotation(val);
         },
+        scale(val) {
+            if (Object.is(this.object3d.scale, val)) return;
+            this.setScale(val);
+        }
     },
 }
