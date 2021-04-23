da02824b21ddf9f1a03ba844e02537ad95f1df77
LiChao7457 <lichao7457@gmail.com>
2021-04-23 11:22:31 +0800

    renew

--- packages/V3dLightAmbient/index.js
+++
@@ -1,7 +0,0 @@
-import V3dAmbient from "./src/V3dLightAmbient"
-
-V3dAmbient.install = function (Vue) {
-    Vue.component(V3dAmbient.name, V3dAmbient);
-};
-
-export default V3dAmbient
--- packages/V3dLightSpot/index.js
+++
@@ -1,7 +0,0 @@
-import V3dLightSpot from "./src/V3dLightSpot"
-
-V3dLightSpot.install = function (Vue) {
-    Vue.component(V3dLightSpot.name, V3dLightSpot);
-};
-
-export default V3dLightSpot
